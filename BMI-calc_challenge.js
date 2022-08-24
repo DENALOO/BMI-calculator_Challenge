c2f2d4bad4fd0211095229a8b2a4cef459073053
Denalo <57454842+DENALOO@users.noreply.github.com>
2022-08-23 20:13:02 -0400

    Add files via upload

---
+++ BMI-calc_challenge.js
@@ -0,0 +1,21 @@
+//Create your function below this line.
+
+function bmiCalculator(weight,height){
+ let bmiCalc = Math.floor(weight/Math.pow(height,2));
+ return bmiCalc;
+}
+
+
+
+
+
+
+
+/******* HINT *******/
+
+//If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); //bmi should equal around 20 in this case.
+
+
+
