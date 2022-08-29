5924ed83dd1a709d917e1c1d78ba093d11b2025f
Denalo <57454842+DENALOO@users.noreply.github.com>
2022-08-29 17:20:27 -0400

    Add files via upload

---
+++ LoveCalculator.js
@@ -0,0 +1,8 @@
+prompt("What is your name?");
+prompt("What is your crushes name?");
+function calcLove(){
+let LovePerc = Math.floor(Math.random()*100+1);
+return(LovePerc);
+}
+
+alert("You and your crush are a " + calcLove()+"%" +" match");
